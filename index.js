7c42807ebb2a148bc3d5062d9b51a4adc3959dc5
Frolov Andrey <frolov@froweb.ru>
2020-08-25 23:25:38 +0300

    creat js and css files

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 'use strict';
-module.exports = FroUp;
-
+module.exports = 
 
 class FroUp {
-  
+  constructor() {
+    this.body = document.querySelector('body');
+    this.banner = document.querySelector('.fro-up');
+  }
+  switchShow() {
+    if (this.banner.classList.contains('visuallyhidden')) {
+      this.banner.classList.remove('visuallyhidden');
+    } else {
+      this.banner.classList.add('visuallyhidden')
+    }
+    this.bodyBlocking();
+  }
+  bodyBlocking() {
+    if (this.body.getAttribute('overflow') === 'hidden') {
+      this.body.removeAttribute('overflow');
+    } else {
+      this.body.setAttribute('overflow', 'hidden');
+    }
+  }
 }
+
