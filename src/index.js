e6ad75f2bbe36462b21e363e6e24b8ba16159e54
Frolov Andrey <frolov@froweb.ru>
2020-10-03 10:37:21 +0300

    initial version

--- src/index.js
+++ src/index.js
@@ -2,18 +2,20 @@
 /**
 * Main class of banner.
 */
-class FroUp {
+export default class FroUp {
   /**
   * Setting basic parameters of the banner.
   * @param {string} id Identifier of the processed banner.
+  * @param {namber} interval
   */
-  constructor(id = null) {
+  constructor(id = null, interval = 0) {
     if (id === null) {
       this.banner = document.querySelector('.fro-up');
     } else {
       this.banner = document.querySelector(`#${id}`);
     }
     this.body = document.querySelector('body');
+    this.interval = interval;
   }
   /**
   * Toggle display/hide banner on page.
@@ -40,11 +42,35 @@ class FroUp {
   * Starting a timer to activate the banner.
   * @param {number} interval time interval before switching on in seconds.
   */
-  timerOn(interval) {
+  timerOn() {
     if (this.banner.classList.contains('visuallyhidden')) {
-      setTimeout(this.switchShow, interval * 1000);
+      setTimeout(() => this.switchShow(), this.interval * 1000);
     }
+  }
+  /**
+  * New.
+  */
+  clickCheck() {
+    // let playId;
+    // if (this.options.interval > 0) {
+    //   playId = this.newPlayId;
+    // }
+    this.banner.addEventListener('click', (e) => {
+      const target = e.target;
+      console.log(target);
+      if (target.classList.contains('fro-up__btn-close')) {
+        this.switchShow();
+      }
+    });
+  }
+  /**
+  * New.
+  */
+  start() {
+    this.banner.classList.add('visuallyhidden');
+    this.timerOn();
+    this.clickCheck();
   }
 }
 
-module.exports = FroUp;
+// module.exports = FroUp;
