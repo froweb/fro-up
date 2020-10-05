440fb66cd549096223286b37be04ebb8f71d9473
Frolov Andrey <frolov@froweb.ru>
2020-10-05 23:50:22 +0300

    code refactoring

--- src/index.js
+++ src/index.js
@@ -7,58 +7,64 @@ export default class FroUp {
   * Setting basic parameters of the banner.
   * @param {string} id Identifier of the processed banner.
   * @param {namber} interval
+  * @param {boolean} block
   */
-  constructor(id = null, interval = 0) {
-    if (id === null) {
-      this.banner = document.querySelector('.fro-up');
-    } else {
-      this.banner = document.querySelector(`#${id}`);
-    }
-    this.body = document.querySelector('body');
-    this.interval = interval;
+  constructor(id, interval = 0, block = false) {
+    this.options = {
+      id: id,
+      interval: interval,
+      block: block,
+    };
+  }
+  /**
+  * New.
+  */
+  get $banner() {
+    return document.querySelector(`#${this.options.id}`);
   }
   /**
   * Toggle display/hide banner on page.
   */
   switchShow() {
-    if (this.banner.classList.contains('visuallyhidden')) {
-      this.banner.classList.remove('visuallyhidden');
+    if (this.$banner.classList.contains('visuallyhidden')) {
+      this.$banner.classList.remove('visuallyhidden');
     } else {
-      this.banner.classList.add('visuallyhidden');
+      this.$banner.classList.add('visuallyhidden');
     }
-    this.bodyBlocking();
+    if (this.options.block === true) {
+      this.bodyBlocking();
+    }
   }
   /**
   * Toggle block/unblock skrolling on page.
   */
   bodyBlocking() {
-    if (this.body.getAttribute('overflow') === 'hidden') {
-      this.body.removeAttribute('overflow');
+    const $body = document.querySelector('body');
+    if ($body.style.overflow === 'hidden') {
+      $body.style.overflow = '';
     } else {
-      this.body.setAttribute('overflow', 'hidden');
+      // $page.setAttribute('overflow', 'hidden');
+      $body.style.overflow = 'hidden';
+      // console.log(overflowValue);
+      // $page.setAttribute('position', 'fixed');
     }
   }
   /**
   * Starting a timer to activate the banner.
-  * @param {number} interval time interval before switching on in seconds.
   */
   timerOn() {
-    if (this.banner.classList.contains('visuallyhidden')) {
-      setTimeout(() => this.switchShow(), this.interval * 1000);
+    if (this.options.interval > 0 &&
+      this.$banner.classList.contains('visuallyhidden')) {
+      setTimeout(() => this.switchShow(), this.options.interval * 1000);
     }
   }
   /**
   * New.
   */
   clickCheck() {
-    // let playId;
-    // if (this.options.interval > 0) {
-    //   playId = this.newPlayId;
-    // }
-    this.banner.addEventListener('click', (e) => {
+    this.$banner.addEventListener('click', (e) => {
       const target = e.target;
-      console.log(target);
-      if (target.classList.contains('fro-up__btn-close')) {
+      if (target.classList.contains('fro-up__close')) {
         this.switchShow();
       }
     });
@@ -67,7 +73,9 @@ export default class FroUp {
   * New.
   */
   start() {
-    this.banner.classList.add('visuallyhidden');
+    if (this.options.interval > 0) {
+      this.$banner.classList.add('visuallyhidden');
+    }
     this.timerOn();
     this.clickCheck();
   }
